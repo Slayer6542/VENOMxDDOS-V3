d43770597c6131a2f2abf8403d2e1d942769adea
jeevan Pujari <124284890+Slayer6542@users.noreply.github.com>
2024-08-01 23:29:14 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7031476424:AAG6bW4N65VCpxyOgdjiWQ6FYbVnGal2uXY'
+ADMIN_ID = '1165523648'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
